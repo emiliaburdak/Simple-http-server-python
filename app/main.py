cbc4c7ac23e1aa694fbabf0cd71068ca9ac8c234
Em <emiliaburdak@gmail.com>
2024-01-16 11:13:15 +0100

    Respond with 200

--- app/main.py
+++ app/main.py
@@ -4,8 +4,21 @@ import socket
 def main():
     print("Logs from your program will appear here!")
 
+    # Bind the socket to a specific address and port
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+
+    # new socket object that the server can use to communicate with the connected client, tuple with address
+    client_socket, address = server_socket.accept()
+
+    # receive data from the client with 1024 bytes (max)
+    request_data = client_socket.recv(1024)
+
+    # send response ok
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.sendall(response)
+
+    # Closing socket
+    client_socket.close()
 
 
 if __name__ == "__main__":
