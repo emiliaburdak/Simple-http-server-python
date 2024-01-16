16be09cf229f79ec295f7113809d496c3557d12f
Em <emiliaburdak@gmail.com>
2024-01-16 12:40:57 +0100

    fix bug- response elements has been joined

--- app/main.py
+++ app/main.py
@@ -20,13 +20,14 @@ def main():
     data_elements_list = request_data.split("\r\n")[0]
     request_method, request_path, request_http_version = data_elements_list.split(" ")
     response_body = request_path.split("/")[2:]
+    response_body_str = "/".join(response_body)
 
     if request_path == "/":
         # send response
         response = "HTTP/1.1 200 OK\r\n\r\n"
     elif request_path.startswith("/echo"):
         # GET /echo/abc HTTP/1.1
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n{response_body}"
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body_str)}\r\n\r\n{response_body_str}"
     else:
         response = "HTTP/1.1 404 Not Found \r\n\r\n"
 
